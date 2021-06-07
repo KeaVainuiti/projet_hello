319347dd0c2d3dccd202ecc3b40022beb0a541d6
KeaVainuiti <69694617+KeaVainuiti@users.noreply.github.com>
2021-06-07 09:46:16 -1000

    Update classlangue.php

--- classlangue.php
+++ classlangue.php
@@ -2,17 +2,27 @@
 require("connexion_bdd.php");
 
 class langue {
-
+    /**
+     * create a language
+     * 
+     * param[string] $name,$translate 
+     * return void
+     */
     public function createLangue($name,$translate){
 
         global $connect_bdd;
 
         $request_insert = "INSERT INTO langue (name,translate) VALUES ('" . $name . "','" . $translate . "')";
-
         $connect_bdd ->query();
     }
 
-
+    
+    /**
+     *  Retrieving all languages
+     * 
+     * param[string] $name,$translate 
+     * return void
+     */
     public function getAllLangue(){
 
         global $connect_bdd ;
@@ -23,6 +33,12 @@ class langue {
         return $get_all_langue;
     }
 
+        /**
+     *  Remove language select from langue
+     * 
+     * param[string] $id
+     * return void
+     */
     public function deleteLangue($id){
 
         global $connect_bdd ;
@@ -31,6 +47,13 @@ class langue {
         $connect_bdd ->query($delete_langue);
     }
 
+      /**
+     *  Update language
+     * 
+     * param[string] $name,$translate,$id
+     * return void
+     */
+        
     public function updateLangue($id,$name,$translate){
 
         global $connect_bdd ;
